a6052ea5fd3ab8dd3071fbdfa79413b1847fc5e6
Felix <sontug9xx@gmail.com>
2025-07-08 21:45:22 +0700

    Push Base

---
+++ src/java/DAO/DBContext.java
@@ -0,0 +1,30 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DBContext {
+    private static final String URL = "jdbc:mysql://localhost:3306/your_db_name";//
+    private static final String USER = "root"; // Thay bằng username MySQL của bạn
+    private static final String PASSWORD = "123456"; // Thay bằng mật khẩu MySQL của bạn
+    
+    protected Connection connection;
+
+    public DBContext() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connection established successfully.");
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Connection failed.");
+        }
+    }
+    
+    public static void main(String[] args) {
+        new DBContext(); // Instantiates DBConnect to test connection
+    }
+}
